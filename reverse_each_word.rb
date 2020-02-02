15cc44c1d18c744fb917b3e0ca6c927a329035b6
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-02 01:36:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,4 +7,9 @@ new_sentence == new_sentence.collect {|sentence| sentence.reverse }
 end
    
   
- 
+ def reverse_each_word(sentence)
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse }
+  new_array.join(" ")
+end
