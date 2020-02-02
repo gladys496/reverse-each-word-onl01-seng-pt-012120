fcc33d048e15b1ca6dca94f3d01c17ee1abcd5a1
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-02 00:28:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
-def reverse_each_word ("Hello there, and  how are you?")
-   
+def reverse_each_word (sentence)
+  new_sentence = []
+sentence.each do |sentences|
+  new_sentence << "Hello there, and how are you?"
    end
    
   end 
