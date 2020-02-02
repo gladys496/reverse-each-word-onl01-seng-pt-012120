ae41a954976d08e891657e971f85a1e11ae70dcb
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-02 01:33:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,6 +10,11 @@
   
  
   
-  
+   def reverse_each_word (sentence)
+  words_sentence = sentence.split(" ")
+  new_sentence = []
+new_sentence = new_sentence.collect {|sentence| sentence.reverse }
+end
+   
   
  
