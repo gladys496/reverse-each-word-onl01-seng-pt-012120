c78e6bb7df16201701b2ccb2577a7117679a46b2
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-02 01:41:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,9 @@
   require "pry"
   
-  def reverse_each_word(sentence)
-  words_array = sentence.split(" ")
-  new_array = []
-  new_array = words_array.collect {|word| word.reverse }
-  new_array.join(" ")
-  binding pry 
-end
-
-
   def reverse_each_word (sentence)
   words_sentence = sentence.split(" ")
   new_sentence = []
 new_sentence = words_sentence.collect {|sentence| sentence.reverse }
+new_sentence.join(" ")
 end
    
